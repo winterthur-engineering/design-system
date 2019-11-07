37dc3c5bd89457e6fd50e74fdc7a9826c97ab1a5
Drew DeCarme <drewdecarme@gmail.com>
2019-11-06 22:15:13 -0500

    feat(part-overrides): Add part-overrides to style default bb parts

---
+++ markup/components/parts/membership.stories.js
@@ -0,0 +1,6 @@
+import "../../../styles/index.scss";
+import membership from "./membership.html";
+
+export default { title: "Part Types|Membership", component: membership };
+
+export const base = () => membership;

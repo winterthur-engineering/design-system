1c1083cad3fec09ba63b4dc3c44292a8b01d0208
Drew DeCarme <drewdecarme@gmail.com>
2019-11-08 08:56:47 -0500

    style(donation): Begin style edits for donation forms

---
+++ src/markup/parts/system/donation.stories.js
@@ -0,0 +1,9 @@
+import "../../../styles/index.scss";
+
+import donationForm from "./donation.html";
+
+export default {
+  title: "Parts|System/Donation"
+};
+
+export const donation = () => donationForm;

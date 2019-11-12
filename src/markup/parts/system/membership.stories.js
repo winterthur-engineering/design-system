a62cde8091000482d51431ef6e3063aba7f3de93
Drew DeCarme <drewdecarme@gmail.com>
2019-11-11 21:48:44 -0500

    style: Stablize donation and membership styles

--- src/markup/parts/system/membership.stories.js
+++ src/markup/parts/system/membership.stories.js
@@ -8,6 +8,8 @@ import FY2015Form from "./membership-FY2015.html";
 import FY2019Form from "./membership-FY2019.html";
 import M15APR5Form from "./membership-M15APR5.html";
 import RenewalFormForm from "./membership-Renewal-form.html";
+import MembershipPage2 from "./membership-page-2.html";
+import pay from "./membership-pay.html";
 
 export default {
   title: "Parts|System/Membership"
@@ -21,3 +23,5 @@ export const FY2015 = () => FY2015Form;
 export const FY2019 = () => FY2019Form;
 export const M15APR5 = () => M15APR5Form;
 export const RenewalForm = () => RenewalFormForm;
+export const Page2 = () => MembershipPage2;
+export const MembershipPay = () => pay;

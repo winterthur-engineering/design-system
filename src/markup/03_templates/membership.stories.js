65436941cf282bac6262af84930d83a50fc66906
Drew DeCarme <drewdecarme@gmail.com>
2019-11-12 23:19:24 -0500

    fix(membership): Extend td as col

--- src/markup/03_templates/membership.stories.js
+++ src/markup/03_templates/membership.stories.js
@@ -1,6 +1,8 @@
 import "../../styles/index.scss";
 import membership from "./membership.html";
+import demoMemebership from "./demo-membership.html";
 
 export default { title: "Templates|Membership" };
 
 export const base = () => membership;
+export const demo = () => demoMemebership;
